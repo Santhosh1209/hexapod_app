652eed958046294b5e04a0baf82cbd930196af61
Santhosh <santhoshgold12@gmail.com>
2023-09-16 18:04:51 +0530

    updated login page - 2

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -14,89 +14,175 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        title: Text('Login Page'),
         centerTitle: true,
         backgroundColor: Color(0xFF6535ee),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 300, // Adjust the width as needed
-              child: DropdownButton<String>(
-                value: selectedRole,
-                onChanged: (String? newValue) {
-                  setState(() {
-                    selectedRole = newValue;
-                  });
-                },
-                items: <String>['Type of Account', 'User', 'Admin'].map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
+        child: Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 20),
+              Text(
+                'Welcome Back!',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
               ),
-            ),
-            SizedBox(height: 20), // Add spacing
-            // TextFormFields for Email and Password
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
+              SizedBox(height: 8),
+              Text(
+                'Enter these details to log into your account',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(height: 60),
+              Container(
+                width: 350,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25.0),
+                  border: Border.all(color: Color(0xFF6535ee)),
+                ),
+                child: DropdownButtonFormField<String>(
+                  value: selectedRole,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedRole = newValue;
+                    });
+                  },
+                  items: <String>['Type of Account', 'User', 'Admin'].map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: 350,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25.0),
+                  border: Border.all(color: Color(0xFF6535ee)),
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: 350,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25.0),
+                  border: Border.all(color: Color(0xFF6535ee)),
+                ),
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                  ),
+                ),
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {
+                  if (selectedRole == 'User') {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NavigationScreen()),
+                    );
+                  } else if (selectedRole == 'Admin') {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => AdminNavigationScreen()),
+                    );
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF6535ee),
+                  padding: EdgeInsets.symmetric(horizontal: 36, vertical: 16), // Adjust padding for a slightly larger button
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                ),
+                child: Text(
+                  'Log In',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              SizedBox(height: 10), // Add spacing between Log In button and Forgot Password button
+              TextButton(
+                onPressed: () {
+                  // Add logic for handling Forgot Password
+                },
+                child: Text(
+                  'Forgot Password?',
+                  style: TextStyle(
+                    color: Color(0xFF6535ee), // Text color in 6535ee color
+                    decoration: TextDecoration.underline, // Underline text
+                  ),
+                ),
+              ),
+              SizedBox(height: 20), // Add spacing between buttons and social media icons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      border: OutlineInputBorder(),
+                  TextButton(
+                    onPressed: () {
+                      //
+                    },
+                    child: Image(
+                      image: AssetImage('images/google.png'),
+                      height: 40,
+                      width: 30,
                     ),
                   ),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    obscureText: true, // For password input
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      border: OutlineInputBorder(),
+                  TextButton(
+                    onPressed: () {
+                      //
+                    },
+                    child: Image(
+                      image: AssetImage('images/facebook-app-symbol.png'),
+                      height: 40,
+                      width: 30,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      //
+                    },
+                    child: Image(
+                      image: AssetImage('images/twitter-sign.png'),
+                      height: 40,
+                      width: 30,
                     ),
                   ),
                 ],
               ),
-            ),
-            SizedBox(height: 20), // Add spacing
-            ElevatedButton(
-              onPressed: () {
-                // You can now access the selectedRole variable to determine the user's choice.
-                // For example, you can check if it's 'User' or 'Admin' and then proceed accordingly.
-                if (selectedRole == 'User') {
-                  // Handle user login
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => NavigationScreen()),
-                  );
-                } else if (selectedRole == 'Admin') {
-                  // Handle admin login
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => AdminNavigationScreen()), // Use the admin navigation for admin
-                  );
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Color(0xFF6535ee), // Change button color
-                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20), // Increase button size
-              ),
-              child: Text(
-                'Log In',
-                style: TextStyle(fontSize: 18), // Increase button text size
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-
-
-
