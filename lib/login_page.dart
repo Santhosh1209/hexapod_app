9c58243ec21ec4cf3bfbcbd5e0d6e4221f2d284e
Santhosh <santhoshgold12@gmail.com>
2023-09-16 17:11:51 +0530

    updated login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,32 +8,61 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  String? selectedRole = 'User'; // Default role is 'User'
+  String? selectedRole = 'Type of Account'; // Default role is 'Type of Account'
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
+        centerTitle: true,
+        backgroundColor: Color(0xFF6535ee),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            DropdownButton<String>(
-              value: selectedRole,
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedRole = newValue;
-                });
-              },
-              items: <String>['User', 'Admin'].map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
+            Container(
+              width: 300, // Adjust the width as needed
+              child: DropdownButton<String>(
+                value: selectedRole,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedRole = newValue;
+                  });
+                },
+                items: <String>['Type of Account', 'User', 'Admin'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
             ),
+            SizedBox(height: 20), // Add spacing
+            // TextFormFields for Email and Password
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    obscureText: true, // For password input
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20), // Add spacing
             ElevatedButton(
               onPressed: () {
                 // You can now access the selectedRole variable to determine the user's choice.
@@ -52,7 +81,14 @@ class _LoginPageState extends State<LoginPage> {
                   );
                 }
               },
-              child: Text('Log In'),
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xFF6535ee), // Change button color
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20), // Increase button size
+              ),
+              child: Text(
+                'Log In',
+                style: TextStyle(fontSize: 18), // Increase button text size
+              ),
             ),
           ],
         ),
@@ -60,7 +96,6 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
-
 
 
 
