e183484d7594600eb6f604b89b87e6ed4e24d96a
Santhosh <santhoshgold12@gmail.com>
2023-09-22 22:15:16 +0530

    added external data to the live tracking page

--- lib/Camera.dart
+++ lib/Camera.dart
@@ -1,15 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class AdminCamera extends StatelessWidget {
+class AdminCamera extends StatefulWidget {
+  @override
+  _AdminCameraState createState() => _AdminCameraState();
+}
+
+class _AdminCameraState extends State<AdminCamera> {
+  late YoutubePlayerController _youtubePlayerController;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the YouTube player controller with your video URL
+    _youtubePlayerController = YoutubePlayerController(
+      initialVideoId: 'YOUR_YOUTUBE_VIDEO_ID',
+      flags: YoutubePlayerFlags(
+        autoPlay: true,
+        mute: false,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Hexapod Camera'),
         centerTitle: true, // Center the title
-        backgroundColor: Color(0xFF6535ee), // Set AppBar background color to #6535ee
+        backgroundColor: Color(0xFF6535EE), // Set AppBar background color to #6535EE
       ),
-      // Content for the Camera Page
+      body: Column(
+        children: [
+          // YouTube Video Player
+          YoutubePlayer(
+            controller: _youtubePlayerController,
+            showVideoProgressIndicator: true,
+            onReady: () {
+              // You can perform actions when the video is ready
+            },
+          ),
+          // Buttons for Taking Photos and Videos
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // Add functionality to take photos
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF6535EE), // Button color
+                ),
+                child: Text(
+                  'Take Photo',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Add functionality to take videos
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF6535EE), // Button color
+                ),
+                child: Text(
+                  'Take Video',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+          // Rest of the camera content
+        ],
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _youtubePlayerController.dispose(); // Dispose of the YouTube player controller
+    super.dispose();
   }
 }
--- lib/LiveTrackingPage.dart
+++ lib/LiveTrackingPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 void main() => runApp(MyApp());
 
@@ -21,16 +22,20 @@ class LiveTrackingPage extends StatefulWidget {
 
 class _LiveTrackingPageState extends State<LiveTrackingPage> {
   List<Marker> markers = []; // List of markers for hexapod locations
+  String selectedGas = 'Nitrogen'; // Selected gas for pie chart
+  double latitude = 13.0827; // Initial latitude
+  double longitude = 80.2707; // Initial longitude
+  bool showExternalData = false;
 
   @override
   void initState() {
     super.initState();
-    // Add initial hexapod location marker
+    // Add initial hexapod location marker in Chennai
     markers.add(
       Marker(
         width: 40.0,
         height: 40.0,
-        point: LatLng(13.0827, 80.2707), // Initial hexapod location (Chennai)
+        point: LatLng(latitude, longitude), // Chennai location
         builder: (ctx) => Container(
           child: Icon(
             Icons.location_on,
@@ -42,36 +47,101 @@ class _LiveTrackingPageState extends State<LiveTrackingPage> {
     );
   }
 
+  void _showPieChart() {
+    List<PieChartSectionData> sections = [];
+
+    if (selectedGas == 'Nitrogen') {
+      sections = [
+        PieChartSectionData(
+          value: 60.0,
+          color: Colors.blue,
+          title: 'Nitrogen',
+          titleStyle: TextStyle(fontSize: 12.0, color: Colors.black),
+        ),
+        PieChartSectionData(
+          value: 30.0,
+          color: Colors.red,
+          title: 'Oxygen',
+          titleStyle: TextStyle(fontSize: 12.0, color: Colors.black),
+        ),
+        PieChartSectionData(
+          value: 5.0,
+          color: Colors.green,
+          title: 'Argon',
+          titleStyle: TextStyle(fontSize: 12.0, color: Colors.black),
+        ),
+        PieChartSectionData(
+          value: 5.0,
+          color: Colors.yellow,
+          title: 'CO2',
+          titleStyle: TextStyle(fontSize: 12.0, color: Colors.black),
+        ),
+      ];
+    }
+
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 300.0,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Text(
+                    'Gas Sensor Composition',
+                    style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Container(
+                  width: 200,
+                  height: 200,
+                  child: PieChart(
+                    PieChartData(
+                      sections: sections,
+                      centerSpaceRadius: 40.0,
+                      sectionsSpace: 0,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  '(Nitrogen 60% : Oxygen 30% : Argon 5% : CO2 5%)',
+                  style: TextStyle(fontSize: 12.0, color: Colors.black),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Live Tracking'),
         backgroundColor: Color(0xFF6535EE),
-        centerTitle: true,
       ),
       body: Column(
         children: [
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Gas Sensor'),
-                ),
-                Text('Temperature: 25°C'),
-                Text('Functional Legs: 6'),
-                Text('Battery: 80%'),
-              ],
+            child: Text(
+              'Latitude: ${latitude.toStringAsFixed(4)}, Longitude: ${longitude.toStringAsFixed(4)}',
+              style: TextStyle(fontSize: 16.0),
             ),
           ),
-          Expanded(
+          Expanded( // Map
             child: FlutterMap(
               options: MapOptions(
-                center: LatLng(13.0827, 80.2707), // Initial map center (Chennai)
-                zoom: 14.0, // Initial zoom level
+                center: LatLng(latitude, longitude),
+                zoom: 13.0,
               ),
               children: [
                 TileLayer(
@@ -79,16 +149,138 @@ class _LiveTrackingPageState extends State<LiveTrackingPage> {
                   subdomains: ['a', 'b', 'c'],
                 ),
                 MarkerLayer(
-                  markers: markers, // Add hexapod location markers here
+                  markers: markers,
                 ),
               ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              'Latitude: ${markers[0].point.latitude.toStringAsFixed(4)}, Longitude: ${markers[0].point.longitude.toStringAsFixed(4)}',
-              style: TextStyle(fontSize: 16.0),
+          Container( // Features
+            color: Colors.white,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      showExternalData = !showExternalData;
+                    });
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(color: Color(0xFF6535EE), width: 1.0),
+                      ),
+                    ),
+                    child: Text(
+                      'External Data',
+                      style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                if (showExternalData)
+                  Column(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedGas = 'Nitrogen';
+                          });
+                          _showPieChart();
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Text(
+                                'Gas Sensor Composition',
+                                style: TextStyle(fontSize: 16.0),
+                              ),
+                              Spacer(),
+                              Icon(
+                                Icons.pie_chart,
+                                size: 24.0,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              'Temperature',
+                              style: TextStyle(fontSize: 16.0),
+                            ),
+                            Spacer(),
+                            Text(
+                              '25°C',
+                              style: TextStyle(fontSize: 16.0),
+                            ),
+                            SizedBox(width: 8.0),
+                            Icon(
+                              Icons.wb_sunny,
+                              size: 20.0,
+                            ),
+                          ],
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          // Handle Battery Percentage feature
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Text(
+                                'Battery Percentage',
+                                style: TextStyle(fontSize: 16.0),
+                              ),
+                              Spacer(),
+                              Text(
+                                '80%',
+                                style: TextStyle(fontSize: 16.0),
+                              ),
+                              SizedBox(width: 8.0),
+                              Icon(
+                                Icons.battery_charging_full,
+                                size: 20.0,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          // Handle Functional Legs feature
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Text(
+                                'Functional Legs',
+                                style: TextStyle(fontSize: 16.0),
+                              ),
+                              Spacer(),
+                              Text(
+                                '6/6',
+                                style: TextStyle(fontSize: 16.0),
+                              ),
+                              SizedBox(width: 8.0),
+                              Icon(
+                                Icons.bug_report_rounded,
+                                size: 20.0,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+              ],
             ),
           ),
         ],
